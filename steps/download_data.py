31e2eb4cf12d01a8454dca9ec7e9433ad40a24ec
flvl9 <lf.villasenor99@gmail.com>
2026-07-28 18:01:10 -0600

    Add step to download data

--- steps/download_data.py
+++ steps/download_data.py
@@ -1,12 +1,39 @@
 import os
-from datasets import load_dataset
+import logging
+from zenml import step
+from dotenv import load_dotenv
+from datasets import load_dataset, DatasetDict
 
-def download_dataset() -> None:
+load_dotenv()
+HF_TOKEN = os.getenv("HF_TOKEN")
+
+logging.basicConfig(level=logging.DEBUG)
+
+@step
+def download_dataset()-> DatasetDict:
+    """
+    Downloads the Plant Village dataset from the HuggingFace hub if the
+    data is not already available. Altohug this dataset is public and does
+    not require authentication, it is recommended to set up a HuggingFace
+    token.
+    """
     current_dir = os.path.dirname(os.path.abspath(__file__))
     project_dir = os.path.dirname(current_dir)
     data_dir = os.path.join(project_dir, "dataset")
+    logging.info("Checking for data...")
 
-    if not os.path.isdir(data_dir):
+    if os.path.isdir(data_dir):
+        logging.info(f"Dataset found on directory: {data_dir}")
+        dataset = load_dataset("GVJahnavi/PlantVillage_dataset", cache_dir=data_dir)
+    else:
+        logging.info("Dataset directory was not found, downloading from the hub...")
+        os.environ["HF_HUB_OFFLINE"] = "0"
         os.mkdir(data_dir)
-        dataset = load_dataset("GVJahnavi/PlantVillage_dataset", split="train")
-        dataset.save_to_disk(dataset_path=data_dir)
+        dataset = load_dataset("GVJahnavi/PlantVillage_dataset", cache_dir=data_dir)
+        logging.info("Dataset downloaded successfully!")
+
+    logging.info("Data retrieved successfully!")
+
+    return dataset
+
+download_dataset()
