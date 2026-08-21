6902bc30a1def2e09567b5db8df503e092462a70
flvl9 <lf.villasenor99@gmail.com>
2026-08-21 16:34:47 -0600

    fix: adds missing description for the data preparation step

--- steps/data_preparation.py
+++ steps/data_preparation.py
@@ -7,6 +7,16 @@ logging.basicConfig(level=logging.DEBUG)
 
 @step(enable_cache=False)
 def get_datamodule(cache_dir: str, data_dir: str, train_batch_size: int, val_batch_size: int) -> pl.LightningDataModule:
+    """
+    Creates a LightningDataModule for the training process.
+    args:
+        cache_dir - The directory in which the dataset cache will be stored.
+        data_dir - The directory in which the dataset will be created and stored.
+        train_batch_size -  The batch size for the train dataloader.
+        val_batch_size - The batch size for the validation dataloader.
+    returns:
+        data_module - The LightningDataModule object required for model training.
+    """
     logging.info("Initializing data preparation...")
     data_module = PlantVillageDatamodule(cache_path=cache_dir,
                                          data_path=data_dir,
